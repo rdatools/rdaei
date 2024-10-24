a6e8fddf9e48784e73aa6ccc2c044f8847f78358
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-10-24 12:19:56 -0700

    WIP

---
+++ rdaei/__init__.py
@@ -0,0 +1,24 @@
+# rdaensemble/general/__init__.py
+
+from .score import (
+    score_ensemble,
+)
+from .minority import (
+    InferredVotes,
+    is_same_candidate_preferred,
+    is_defined_opportunity_district,
+    count_defined_opportunity_districts,
+    load_EI_votes,
+    aggregate_votes_by_district,
+)
+from .notable_maps import (
+    id_notable_maps,
+    ratings_dimensions,
+    ratings_indexes,
+    better_map,
+    qualifying_map,
+)
+from .metautils import shared_metadata, ensemble_metadata, scores_metadata
+from .utils import make_plan, plan_from_ensemble
+
+name: str = "general"
